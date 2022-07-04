5d608c2a05e14a7d0058123fe56346873c72f6f6
realhee <wlsgml5628@gmail.com>
2022-07-04 23:53:12 +0900

    4-2 뒤집은 소수 - isPrime 복잡도 줄이기

--- js-algorithm/04-BruteForce/4-2.js
+++ js-algorithm/04-BruteForce/4-2.js
@@ -3,15 +3,18 @@
 // 문자열 반대로 정렬하기 - reverse()
 // 소수 구하기 - isPrime() 함수 정의
 function isPrime(num) {
+  // (조건 1) 소수는 1보다 큰 정수
+  if (num === 1) return false;
   // 소수 : 1과 자기 자신만으로 나누어 떨어지는 수
-  // (조건 1) i < num
-  for (let i = 2; i < num; i++) {
+  // (조건 2) i < num / 2
+  // 어차피 2x8, 8x2는 같으므로 시간복잡도를 위해 절반만 구해도 된다!
+  // 제곱근도 가능 - Math.sqrt()
+  for (let i = 2; i < parseInt(num / 2); i++) {
     if (num % i === 0) {
       return false;
     }
   }
-  // (조건 2) 소수는 1보다 큰 정수
-  return num > 1;
+  return true;
 }
 function solution(arr) {
   let answer = [];
