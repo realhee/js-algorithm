0c7c3243ae7167970d3f74f38fb5c784d35224ec
realhee <wlsgml5628@gmail.com>
2022-07-06 23:59:53 +0900

    code formatting

--- js-algorithm/03-StringSearch/3-1.js
+++ js-algorithm/03-StringSearch/3-1.js
@@ -1,11 +1,11 @@
 // 3-1 회문문자열
 function solution(str) {
-    const newStr = str.toUpperCase();
-    for(let i=0; i < Math.floor(newStr.length/2); i++) {
-        if(newStr[newStr.length-i-1] != newStr[i]) return "NO";
-    }
-    return "YES";
+  const newStr = str.toUpperCase();
+  for (let i = 0; i < Math.floor(newStr.length / 2); i++) {
+    if (newStr[newStr.length - i - 1] != newStr[i]) return "NO";
+  }
+  return "YES";
 }
 
-let str="gooG";
+let str = "gooG";
 console.log(solution(str)); // result : "YES"
