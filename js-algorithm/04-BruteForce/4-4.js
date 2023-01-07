dba9e4ee520a73c848007c799cfe770922008973
realhee <wlsgml5628@gmail.com>
2023-01-07 21:48:11 +0900

    4-4 졸업선물

---
+++ js-algorithm/04-BruteForce/4-4.js
@@ -0,0 +1,21 @@
+// 4-4 졸업선물
+// 학생 수 N (1~1000)
+// 예산 M (1~100000000)
+
+function solution(arr) {
+  let answer = "answer";
+  for (let i = 0; i < 5; i++) {
+    console.log("arr[0] : ", arr[0]);
+    console.log("arr[1] : ", arr[1]);
+  }
+  return answer;
+}
+
+let arr = [
+  [6, 6],
+  [2, 2],
+  [4, 3],
+  [4, 5],
+  [10, 3],
+];
+console.log(solution(28, arr)); // result : 4
